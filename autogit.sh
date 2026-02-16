0211a8d2294efd2e3842ea0364aa7b867d3d55a7
Hilario Filho <hilariospol@gmail.com>
2026-02-15 22:54:59 -0300

    colocando intervalo entre os commits

--- autogit.sh
+++ autogit.sh
@@ -4,9 +4,14 @@ PASTA_REPOSITORIO="$HOME/pastaTeste_automatizarGit"
 MENSAGEM_COMMIT="commit automático"
 BRANCH="main"
 
+INTERVALO_COMMIT = 600 #10 minutos em s
+ULTIMO_COMMIT = 0
+HOUVE_ALTERACAO = false
+
 cd "$PASTA_REPOSITORIO" || exit 1
 
 echo "Monitorando alterações em $PASTA_REPOSITORIO ..."
+echo "Commits automáticos a cada 10 minutos, se houver mudanças"
 
 inotifywait -m -r \
   --exclude '\.git' \
@@ -16,14 +21,23 @@ inotifywait -m -r \
   "$PASTA_REPOSITORIO" |
 while read -r caminho evento arquivo; do
     echo "Alteração detectada: $evento em $arquivo"
-    sleep 1
-    git add .
+    HOUVE_ALTERACAO = true
 
-    if git diff --cached --quiet; then
-        echo "nenhuma mudança real para commit"
-        continue
+    AGORA=$(date +%s)
+    DIFERENCA=$((AGORA - ULTIMO_COMMIT))
+
+    if ["$HOUVE_ALTERACAO" = true] && ["$DIFERENCA" -ge "$INTERVALO_COMMIT"]; then
+        echo "Intervalo atingido. Commitando"
+
+        git add .
+
+        if git diff --cached --quiet; then
+            echo "nenhuma mudança real para commit"
+        else
+            git commit -m "$MENSAGEM_COMMIT"
+            git push -u origin "$BRANCH"
+	    ULTIMO_COMMIT=$AGORA
+	    HOUVE_ALTERACAO=false
+	fi
     fi
-
-    git commit -m "$MENSAGEM_COMMIT"
-    git push -u origin "$BRANCH"
 done
